73cbc83682624e64673a12529a2cd6c3246d429d
michbur <michalburdukiewicz@gmail.com>
2018-12-07 14:50:48 +0100

    added frequentist approach

--- data-simulation.R
+++ data-simulation.R
@@ -1,1 +1,21 @@
-x <- rnorm(100)
+library(dplyr)
+library(broom)
+
+dat <- rbind(rnorm(n = 240, mean = 0) %>% 
+               matrix(nrow = 10) %>% 
+               data.frame %>% 
+               mutate(status = "A"),
+             rnorm(n = 240, mean = 1.5) %>% 
+               matrix(nrow = 10) %>% 
+               data.frame %>% 
+               mutate(status = "B"))
+
+lapply(paste0("X", 1L:24), function(i)
+  t.test(x = dat[dat[["status"]] == "A", i], y = dat[dat[["status"]] == "B", i]) %>% 
+    tidy %>% 
+    mutate(param = i) %>% 
+    select(param, p.value)
+) %>% bind_rows() %>% 
+  mutate(ap.value = p.adjust(p.value, method = "BY"))
+
+
