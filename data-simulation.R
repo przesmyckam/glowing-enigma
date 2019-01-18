ca25f37ff491c4d75825d6c61a0d3cdd335233e6
michbur <michalburdukiewicz@gmail.com>
2019-01-18 15:06:19 +0100

    working on data simulation

--- data-simulation.R
+++ data-simulation.R
@@ -1,70 +1,7 @@
 library(dplyr)
 library(broom)
+library(BEST)
 set.seed(1410)
-dat2 <- cbind(rnorm(n = 50*24, mean = 0) %>% 
-                matrix(nrow = 24) %>% 
-                data.frame,
-              rnorm(n = 50*24, mean = 1.5) %>% 
-                matrix(nrow = 24) %>% 
-                data.frame)
-ncol(dat2)
-dat2[,"status"]
-lista<-lapply(paste0("X", 1L:100), function(i,j)
-  t.test(x = dat2[, i], y = dat2[, j]) %>% 
-    tidy %>% 
-    mutate(param = i) %>% 
-    select(param=param, p.value=p.value)
-) %>% bind_rows() %>% 
-  mutate(ap.value = p.adjust(p.value, method = "BY"))
-#powiekszyc zbior parametrow, 24 pacjenrow w obu grupach, 100 parametrow, 50 istotnych par, 50 nieist, 
-#losujemy seed
-#nowedat, p.value
-lista
-
-
-
-
-raw_dat <-cbind(rbind(rnorm(n = 50*24, mean = 0) %>% 
-                    matrix(nrow = 24) %>% 
-                    data.frame,
-                  rnorm(n = 50*24, mean = 1.5) %>% 
-                    matrix(nrow = 24) %>% 
-                    data.frame),
-            rnorm(n = 100*24, mean = 0) %>% 
-              matrix(nrow = 48) %>% 
-              data.frame)
-
-colnames(raw_dat) <- paste0("X", 1L:100)
-
-dat <- mutate(raw_dat, status = c(rep("A", 24), rep("B", 24)))
-dim(dat)
-lapply(paste0("X", 1L:100), function(i)
-  t.test(x = dat[dat[["status"]] == "A", i], y = dat[dat[["status"]] == "B", i]) %>% 
-    tidy %>% 
-    mutate(param = i) %>% 
-    select(param, p.value)
-) %>% bind_rows() %>% 
-  mutate(ap.value = p.adjust(p.value, method = "BY"))
-#ile z pwartsoc istotnych, ile nie, rozklad p wartosci dla obu grup parametrów
-
-dat1 <- t(rbind(rnorm(n = 240, mean = 0) %>% 
-                  matrix(nrow = 10) %>% 
-                  data.frame %>% 
-                  mutate(status = "A"),
-                rnorm(n = 240, mean = 1.5) %>% 
-                  matrix(nrow = 10) %>% 
-                  data.frame %>% 
-                  mutate(status = "B")))
-
-lapply(paste0("X", 1L:24), function(i)
-  t.test(x = dat[dat[["status"]] == "A", i], y = dat[dat[["status"]] == "B", i]) %>% 
-    tidy %>% 
-    mutate(param = i) %>% 
-    select(param, p.value)
-) %>% bind_rows() %>% 
-  mutate(ap.value = p.adjust(p.value, method = "BY"))
-#ile z pwartsoc istotnych, ile nie, rozklad p wartosci dla obu grup parametrów
-
 
 dat1 <- rbind(rnorm(n = 240, mean = 0) %>% 
                 matrix(nrow = 10) %>% 
@@ -75,13 +12,26 @@ dat1 <- rbind(rnorm(n = 240, mean = 0) %>%
                 data.frame %>% 
                 mutate(status = "B"))
 
-lapply(paste0("X", 1L:24), function(i)
+dat_different <- lapply(paste0("X", 1L:24), function(i) {
+  best_res <- BESTmcmc(y1 = dat[dat[["status"]] == "A", i], y2 = dat[dat[["status"]] == "B", i])
+  eff_size <- summary(best_res)["effSz", "mean"]
   t.test(x = dat[dat[["status"]] == "A", i], y = dat[dat[["status"]] == "B", i]) %>% 
     tidy %>% 
     mutate(param = i) %>% 
-    select(param, p.value)
-) %>% bind_rows() %>% 
+    select(param, p.value) %>% 
+    mutate(eff_size = eff_size)
+}) %>% bind_rows() %>% 
   mutate(ap.value = p.adjust(p.value, method = "BY"))
-#ile z pwartsoc istotnych, ile nie, rozklad p wartosci dla obu grup parametrów
+
+dat_same <- lapply(paste0("X", 1L:24), function(i) {
+  best_res <- BESTmcmc(y1 = dat[dat[["status"]] == "A", i], y2 = resample(dat[dat[["status"]] == "A", i]))
+  eff_size <- summary(best_res)["effSz", "mean"]
+  t.test(x = dat[dat[["status"]] == "A", i], y = resample(dat[dat[["status"]] == "A", i])) %>% 
+    tidy %>% 
+    mutate(param = i) %>% 
+    select(param, p.value) %>% 
+    mutate(eff_size = eff_size)
+}) %>% bind_rows() %>% 
+  mutate(ap.value = p.adjust(p.value, method = "BY"))
 
 
