30aedfdce2d3f6f25e25135281995a0a5f92312c
michbur <michalburdukiewicz@gmail.com>
2019-05-16 22:23:19 +0200

    updated data simulation scheme

--- data-simulation-scheme2.R
+++ data-simulation-scheme2.R
@@ -1,4 +1,5 @@
 library(BEST)
+library(pbapply)
 
 # ns - number of samples in each block (A or B)
 
@@ -9,27 +10,39 @@ random_samples <- function(ns) {
                    values = rnorm(10*ns, mean = 3, sd = 1)))
 }
 
+adjust_subset <- function(ns, x)
+  lapply(2L:ns, function(ith_n) {
+    ids <- sample(1L:ns, ith_n)
+    subset_x <- x[grepl(pattern = paste0("[", paste0(ids, collapse = ""), "]"), x = x[["gr1"]]) & 
+                    grepl(pattern = paste0("[", paste0(ids, collapse = ""), "]", collapse = ""), x = x[["gr2"]]), ]
+    subset_x[["hochberg"]] <- p.adjust(subset_x[["p.value"]], method = "BH")
+    subset_x[["yekutieli"]] <- p.adjust(subset_x[["p.value"]], method = "BY")
+    subset_x[["ns"]] <- ith_n
+    subset_x[, c("same", "effSz", "hochberg", "yekutieli", "ns")]
+  })
+
 do_simulation <- function(ns) {
   randomed_samples <- random_samples(ns)
   
   all_combn <- combn(levels(randomed_samples[["block_id"]]), 2, simplify = FALSE)
   
-  res_t <- do.call(rbind, lapply(all_combn, function(ith_combn) {
+  res_t <- do.call(rbind, pblapply(all_combn, function(ith_combn) {
     
-    best_res <- BESTmcmc(randomed_samples[randomed_samples[["block_id"]] == ith_combn[1], "values"],
-                         randomed_samples[randomed_samples[["block_id"]] == ith_combn[2], "values"])
+    best_res <- suppressMessages(BESTmcmc(randomed_samples[randomed_samples[["block_id"]] == ith_combn[1], "values"],
+                                          randomed_samples[randomed_samples[["block_id"]] == ith_combn[2], "values"]))
     
-    data.frame(same = substr(ith_combn[1], 0, 1) == substr(ith_combn[2], 0, 1),
+    data.frame(gr1 = ith_combn[1],
+               gr2 = ith_combn[2],
+               same = substr(ith_combn[1], 0, 1) == substr(ith_combn[2], 0, 1),
                effSz = summary(best_res)["effSz", "mean"], 
                p.value = t.test(randomed_samples[randomed_samples[["block_id"]] == ith_combn[1], "values"],
                                 randomed_samples[randomed_samples[["block_id"]] == ith_combn[2], "values"], 
                                 paired = FALSE)[["p.value"]])
   }))
-  
-  res_t[["hochberg"]] <- p.adjust(res_t[["p.value"]], method = "BH")
-  res_t[["yekutieli"]] <- p.adjust(res_t[["p.value"]], method = "BY")
-  res_t[["ns"]] <- ns
-  res_t
 }
 
-whole_simultion <- lapply(2:3, do_simulation)
+set.seed(15390)
+
+res <- do_simulation(120)
+save(res, file = "simulation.RData")
+save(res, file = "/home/michal/Dropbox/PepArray_results/simulation.RData")
